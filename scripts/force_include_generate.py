ecdd81a3b67068a60fb41454dd0bc1b9d68652f7
Miku <26039249+xMikux@users.noreply.github.com>
2024-07-05 07:17:51 +0000

    ci: wrong env

--- scripts/force_include_generate.py
+++ scripts/force_include_generate.py
@@ -1,7 +1,7 @@
 import os
 
 def generate_include_list(include_list):
-    with open(os.environ["GITHUB_STEP_SUMMARY"], "w") as env:
+    with open(os.environ["GITHUB_OUTPUT"], "w") as env:
         for i in include_list:
             env.write(f"['{i}']\n")
             env.write("force_include = true")
