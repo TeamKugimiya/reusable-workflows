440dddd8468f3e9bf163171b830d0e7061dc12af
Miku <26039249+xMikux@users.noreply.github.com>
2024-07-05 06:39:37 +0000

    ci: fix force include and test

---
+++ scripts/force_include_generate.py
@@ -0,0 +1,22 @@
+import os
+
+def generate_include_list(include_list):
+    with open(os.environ["GITHUB_STEP_SUMMARY"], "w") as env:
+        for i in include_list:
+            env.write(f"['{i}']\n")
+            env.write("force_include = true")
+
+    print("Force Inlucde Files:")
+    for i in include_list:
+        print(f"['{i}']")
+        print("force_include = true")
+
+def main():
+    include_list = os.environ.get("force_include_files")
+
+    if include_list is not None:
+        generate_include_list(include_list)
+    else:
+        print("Array is Empty!")
+
+main()
