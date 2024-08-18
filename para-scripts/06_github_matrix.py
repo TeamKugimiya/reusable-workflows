a893a59f2f35d60f4371b8e840d3c72d5db38074
Miku <26039249+xMikux@users.noreply.github.com>
2024-08-18 09:48:32 +0000

    ci: test matrix

--- para-scripts/06_github_matrix.py
+++ para-scripts/06_github_matrix.py
@@ -1,3 +1,4 @@
+import json
 from utils import load_tomldata, github_write_step_output
 from pathlib import Path
 from loguru import logger
@@ -8,9 +9,12 @@ def matrix_generate(version_data: dict) -> dict:
     matrix_versions = {
         "include": [
             {
-                "version": v.get("mc_version"),
-                "pack_format": v.get("mc_pack_format"),
-                "supported_formats_min": v.get("mc_supported_formats_min")
+                "mc_version": v.get("mc_version"),
+                "mc_pack_format": v.get("mc_pack_format"),
+                "mc_supported_formats_min": v.get("mc_supported_formats_min"),
+                "mc_supported_formats_max": v.get("mc_supported_formats_max"),
+                "modrinth_version": v.get("modrinth_version"),
+                "directory_path": v.get("directory_path")
             }
             for v in version_data.get("versions", [])
         ]
@@ -21,7 +25,8 @@ def matrix_generate(version_data: dict) -> dict:
 def main():
     config_data = load_tomldata(CONFIG_PATH)
     matrix_json = matrix_generate(config_data)
-    # github_write_step_output("matrix_json", matrix_json)
-    # output = json.dumps(result, separators=(",", ":"), ensure_ascii=False).replace("'", "\"")  # noqa: E501
+    # matrix_yaml_json = json.dumps(matrix_json, separators=(",", ":"), ensure_ascii=False).replace("'", "\"")
+    # print(matrix_yaml_json)
+    github_write_step_output("matrix_json", matrix_json)
 
 main()
