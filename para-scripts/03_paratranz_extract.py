b513f685ef3b01ba8514135d367edfc2c8ae3c84
Miku <26039249+xMikux@users.noreply.github.com>
2024-09-08 11:16:58 +0000

    fix: duplicate .json.json

--- para-scripts/03_paratranz_extract.py
+++ para-scripts/03_paratranz_extract.py
@@ -21,9 +21,19 @@ def remove_utf8(path: Path):
     logger.info("Remove ut8 folder...")
     shutil.rmtree(path)
 
+def duplicate_filename_fix(raw_workdir_path: Path):
+    logger.info("Fixing duplicate json...")
+    for file_path in raw_workdir_path.rglob("*.json.json"):
+        new_file_name = file_path.name.replace(".json.json", ".json")
+        new_file_path = file_path.with_name(new_file_name)
+        logger.success(f"Renamed {new_file_name}.")
+        logger.debug(f"- path {new_file_path}")
+        file_path.rename(new_file_path)
+
 def main():
     archive_extract(WORKDIR_FILE_PATH, WORKDIR_FOLDER_PATH)
     remove_utf8(WORKDIR_FOLDER_PATH.joinpath("utf8"))
     fix_paths(WORKDIR_FOLDER_PATH.joinpath("raw/MultiVersions"), WORKDIR_FOLDER_PATH.joinpath("MultiVersions"), WORKDIR_FOLDER_PATH.joinpath("raw"))
+    duplicate_filename_fix(WORKDIR_FOLDER_PATH.joinpath("MultiVersions"))
 
 main()
